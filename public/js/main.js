6eb3d4d7870b4b10a7c31722ca66f21f816e7a75
Asheepun <pussagussa@gmail.com>
2019-02-07 18:19:00 +0100

    reworked hellevator level

--- public/js/main.js
+++ public/js/main.js
@@ -121,16 +121,16 @@ Promise.all([
 		},
 		state: undefined,
 		context: vec(0, 0),
-		currentLevel: 17,
+		currentLevel: 24,
 		volume: 1,
 		saveProgress: true,
 		deaths: 0,
 	};
 
-	GAME.state = GAME.states.setupStartscreen;
+	//GAME.state = GAME.states.setupStartscreen;
 
 	//GAME.audio.setVolume(0);
-	//localStorage.currentLevel = GAME.currentLevel;
+	localStorage.currentLevel = GAME.currentLevel;
 	
 	if(storageAvailable()){
 		if(localStorage.currentLevel === undefined)
